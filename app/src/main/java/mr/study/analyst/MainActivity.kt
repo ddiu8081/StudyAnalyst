eb22c6bac67f236088d14be664bafd3be916f135
ddiu8081 <ddiu8081@gmail.com>
2018-05-13 11:08:27 +0800

    Move to Manjaro Linux.

--- app/src/main/java/mr/study/analyst/MainActivity.kt
+++ app/src/main/java/mr/study/analyst/MainActivity.kt
@@ -3,7 +3,9 @@ package mr.study.analyst
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.act
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         QMUIStatusBarHelper.translucent(this) //沉浸化状态栏
         QMUIStatusBarHelper.setStatusBarLightMode(act) //设置状态栏黑色字体图标
+
+        toast("linux test")
     }
 }
